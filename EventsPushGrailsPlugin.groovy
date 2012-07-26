4ba7d55d4e1afd329ad302ff79481ed368a5952b
Stephane Maldini <smaldini@vmware.com>
2012-07-26 11:28:42 +0100

    wrong inline plugin dep

--- EventsPushGrailsPlugin.groovy
+++ EventsPushGrailsPlugin.groovy
@@ -26,7 +26,7 @@ import org.springframework.util.ClassUtils
 
 class EventsPushGrailsPlugin {
     // the plugin version
-    def version = "1.0.M1"
+    def version = "1.0.M1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
